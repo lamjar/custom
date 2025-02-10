27f2be108412d403dbf2bd5c566af8db8352535d
a85407538 <a85407538@gmail.com>
2025-02-10 20:30:18 +0000

    V1.1

--- script.js
+++ script.js
@@ -79,93 +79,20 @@ function addMessage(sender, message) {
 
     const messageContent = document.createElement('div');
     messageContent.classList.add('message-content');
-    messageContent.innerHTML = formatMarkdown(message);
+    messageContent.innerHTML = formatMarkdown(message); // Formater le Markdown
     messageElement.appendChild(messageContent);
     chatBox.appendChild(messageElement);
     chatBox.scrollTop = chatBox.scrollHeight;
+
+    // Appliquer la coloration syntaxique aux blocs de code
+    document.querySelectorAll('pre code').forEach((block) => {
+        hljs.highlightBlock(block);
+    });
 }
 
 function formatMarkdown(message) {
-    // Titres
-    message = message.replace(/^# (.*$)/gm, '<h1>$1</h1>');
-    message = message.replace(/^## (.*$)/gm, '<h2>$1</h2>');
-    message = message.replace(/^### (.*$)/gm, '<h3>$1</h3>');
-
-    // Texte en gras et souligné
-    message = message.replace(/(\*\*|__)(.*?)\1/g, '<strong><u>$2</u></strong>');
-
-    // Italique
-    message = message.replace(/\*(.*?)\*/g, '<em>$1</em>');
-    message = message.replace(/_(.*?)_/g, '<em>$1</em>');
-
-    // Barré
-    message = message.replace(/~~(.*?)~~/g, '<del>$1</del>');
-
-    // Listes non ordonnées
-    message = message.replace(/^\* (.*$)/gm, '<ul><li>$1</li></ul>');
-    message = message.replace(/^\d+\. (.*$)/gm, '<ol><li>$1</li></ol>');
-
-    // Blocs de code avec couleurs ANSI
-    message = message.replace(/```([\s\S]*?)```/g, (match, p1) => {
-        return `<pre><code>${ansiToHtml(p1)}</code></pre>`;
-    });
-
-    // Code inline
-    message = message.replace(/`(.*?)`/g, '<code>$1</code>');
-
-    // Liens
-    message = message.replace(/\[(.*?)\]\((.*?)\)/g, '<a href="$2" class="reference">$1</a>');
-
-    // Images
-    message = message.replace(/!\[(.*?)\]\((.*?)\)/g, '<img src="$2" alt="$1">');
-
-    // Citations
-    message = message.replace(/^> (.*$)/gm, '<blockquote>$1</blockquote>');
-
-    // Lignes horizontales
-    message = message.replace(/^---$/gm, '<hr>');
-    message = message.replace(/^\*\*\*$/gm, '<hr>');
-    message = message.replace(/^___$/gm, '<hr>');
-
-    // Références avec indices
-    message = message.replace(/\[ref(\d+)\]/g, (match, p1) => {
-        return `<sup><a href="#ref${p1}" class="reference-index">[${p1}]</a></sup>`;
-    });
-
-    return message;
-}
-
-// Fonction pour convertir les codes ANSI en HTML
-function ansiToHtml(text) {
-    const ansiColors = {
-        '30': 'ansi-black',
-        '31': 'ansi-red',
-        '32': 'ansi-green',
-        '33': 'ansi-yellow',
-        '34': 'ansi-blue',
-        '35': 'ansi-magenta',
-        '36': 'ansi-cyan',
-        '37': 'ansi-white',
-        '90': 'ansi-bright-black',
-        '91': 'ansi-bright-red',
-        '92': 'ansi-bright-green',
-        '93': 'ansi-bright-yellow',
-        '94': 'ansi-bright-blue',
-        '95': 'ansi-bright-magenta',
-        '96': 'ansi-bright-cyan',
-        '97': 'ansi-bright-white',
-    };
-
-    return text.replace(/\x1b\[([\d;]+)m/g, (match, p1) => {
-        const codes = p1.split(';');
-        let html = '';
-        codes.forEach(code => {
-            if (ansiColors[code]) {
-                html += `<span class="${ansiColors[code]}">`;
-            }
-        });
-        return html;
-    }).replace(/\x1b\[0m/g, '</span>');
+    // Utiliser marked.js pour convertir le Markdown en HTML
+    return marked.parse(message);
 }
 
 function addTypingIndicator() {
